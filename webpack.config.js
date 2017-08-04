b3d6914e9687966b8c3031e7f61913d230ff0a8e
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-08-04 23:59:49 +0800

    Add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,24 @@ module.exports = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 1 },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  require('autoprefixer')(),
+                  require('postcss-custom-media')(),
+                  require('postcss-custom-properties')(),
+                  require('postcss-nesting')(),
+                ],
+              },
+            },
+          ],
         }),
       },
     ],
